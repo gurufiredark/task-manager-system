1f6818d49c38d88e69f0697f93c282f758809f53
Gabriel Rodrigues <gabrielrodriguesguru@gmail.com>
2026-02-01 16:49:59 -0300

    feat: configurar Program.cs e injeção de dependencia

--- TaskManager.Api/Program.cs
+++ TaskManager.Api/Program.cs
@@ -1,41 +1,46 @@
+using TaskManager.Api.Repositories;
+using TaskManager.Api.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// Adicionar serviços
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Injeção de Dependência
+builder.Services.AddSingleton<ITaskRepository, JsonTaskRepository>();
+builder.Services.AddScoped<ITaskService, TaskService>();
+
+// CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+// Pipeline HTTP
+app.UseSwagger();
+app.UseSwaggerUI(options =>
 {
-    app.MapOpenApi();
-}
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "Task Manager API");
+    options.RoutePrefix = string.Empty;
+});
 
+app.UseCors("AllowAll");
 app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+var logger = app.Services.GetRequiredService<ILogger<Program>>();
+logger.LogInformation("Task Manager API iniciada!");
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+public partial class Program { }
--- TaskManager.Api/obj/Debug/net10.0/TaskManager.Api.AssemblyInfo.cs
+++ TaskManager.Api/obj/Debug/net10.0/TaskManager.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManager.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd1b950f90a54a55a1e6f5d5fdafe695fced182c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0527c0f1b9b100e266e30f9587b8619dc6d9728c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManager.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManager.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
